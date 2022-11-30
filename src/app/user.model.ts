0642e0130827171c7ccb5906ae1a2c891fa99099
Hyun Kim <kmasterh@gmail.com>
2022-11-30 13:17:36 +0100

    Add properties into class

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -1,10 +1,11 @@
-// User Singup: RWTH Email로 최초 1회 인증(인증코드 발행 및 입력)
-// User Login: Google Authentification(SocialAuthServiceConfig 모듈)
+// User Singup: Authentification via RWTH mail(Verification code sent and enter the code)
+// User Login: Google Authentification(SocialAuthServiceConfig Module from Aungular)
 
 export class User {
   constructor(
     userId: string,  // user id
-    nickName: string,  // nickname: 게시물에 표시되는 이름 / 수정 가능
-    email: string  // 최초 인증 후 기록
+    userkName: string,  // userName: name showed on card / editable
+    email: string,  // RWTH email(store after verify for sing up)
+    isSignedup: boolean  // information required when loggin in
   ){}
 }
