7258d05a8a66cae827dcf0ce71a3bfff8416c299
minseon.kim <minseon.kim@rwth-aachen.de>
2023-01-13 15:55:32 +0100

    Implement email verification fucntions

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+import { Signup2Component } from '../signup2/signup2.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-signup',
@@ -7,14 +11,41 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  email: string = '';
 
-  constructor(public dialogRef: MatDialogRef<SignupComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<SignupComponent>,
+    public auth: AuthService,
+    public router: Router,
+    public matDialog: MatDialog,
+    ) { }
 
   ngOnInit(): void {
   }
 
   closeModal() {
     this.dialogRef.close();
+  }
+
+  verificationEmail(){
+    console.log(this.email);
+    this.auth.verificationEmail(this.email).subscribe(data => {
+      console.log(data);
+      console.log(data.status);
+      if(data.message === `${this.email}으로 인증번호를 전송 하였습니다.`){
+        this.openSignup2Modal();
+      }
+    }
+    )
+  }
+
+  openSignup2Modal() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.id = "signup2-component";
+    dialogConfig.height = "600px";
+    dialogConfig.width = "800px";
+    const modalDialog = this.matDialog.open(Signup2Component, dialogConfig);
   }
 
 }
