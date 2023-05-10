f20bb31f1850c7b016077133fe41927733bba87b
Sungdae Kim <hotag9@gmail.com>
2023-05-10 16:26:47 +0200

    refactor router/card.js

--- backend/router/card.js
+++ backend/router/card.js
@@ -1,82 +1,17 @@
 import express from 'express';
 
-import * as cardRepository from '../database/card.js';
+import * as cardController from '../controller/card.js';
+import * as commentController from '../controller/comment.js';
 
 import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/:id', isAuth, async (req, res) => {
-  const id = req.params.id;
-  const card = await cardRepository.getCard(id);
-
-  card.views = card.views + 1;
-  card.save();
-
-  if(card) {
-    res.status(200).json(card);
-  } else {
-    res.status(404).json({ message: 'card not found' });
-  }
-});
-
-
-router.post('/', isAuth, async (req, res) => {
-  const { title, text, category, term, course } = req.body;
-  const { googleID, username, avatarUrl } = req.user;
-  const card = await cardRepository.create(title, text, category, username, avatarUrl, term, course, googleID);
-  res.status(201).json(card);
-});
-
-router.put('/:id', isAuth, async (req, res) => {
-  const { title, text, username, avatarUrl, category, term, course, views } = req.body;
-  const id = req.params.id;
-  const card = await cardRepository.getCard(id);
-  
-  if(!card){
-    res.status(404).json({ message: `card not found :${id}` });
-  } else if(card.username != req.user.username){
-    res.status(403).json({ message: 'user is not author' });
-  } else {
-    const updated = await cardRepository.update(id, title, text, username, avatarUrl, category, term, course, views);
-    res.status(200).json(updated);
-  }
-});
-
-router.put('/views/:id', async (req, res) => {
-  const id = req.params.id;
-  const views = req.body;
-  
-  const newCard = cardRepository.viewsUpdate(id, views);
-
-  if (!newCard) {
-    res.status(404).json({ message: `card not found :${id}` });
-  } 
-  res.status(200).json;
-})
-
-router.delete('/:id', isAuth, async (req, res) => {
-  const id = req.params.id;
-  const { googleID, username } = req.user;
-  const card = await cardRepository.getCard(id, googleID);
-
-  if(!card){
-    res.status(404).json({ message: `card not found :${id}` });
-  } else if(card.username != username){
-    res.status(403).json({ message: 'user is not author' });
-  } else {
-    await cardRepository.remove(id);
-    res.sendStatus(204);
-  }
-});
-
-router.post('/:id/comment', isAuth, async (req, res) => {
-  const cardId = req.params.id;
-  const text = req.body.text;
-  const { googleID, username } = req.user;
-
-  const comment = await cardRepository.commentCreate(cardId, text, username, googleID);
-  res.status(201).json(comment);
-});
+router.get('/:id', isAuth, cardController.getCard);
+router.post('/', isAuth, cardController.createCard);
+router.put('/:id', isAuth, cardController.updateCard);
+router.delete('/:id', isAuth, cardController.deleteCard);
+router.put('/views/:id', cardController.viewsUpdate);
+router.post('/:id/comment', isAuth, commentController.createComment);
 
 export default router;
