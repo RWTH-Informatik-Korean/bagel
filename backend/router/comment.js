9545d838b145c80c6da8e9ae98e2b80122c2cc7b
Sungdae Kim <hotag9@gmail.com>
2023-05-10 16:32:22 +0200

    refactor router/comment.js

--- backend/router/comment.js
+++ backend/router/comment.js
@@ -1,42 +1,12 @@
 import express from 'express';
 
-import * as cardRepository from '../database/card.js';
+import * as commentController from '../controller/comment.js';
 
 import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.put('/:id', async (req, res) => {
-  const id = req.params.id;
-  const text = req.body.text;
-  const username = req.user.username;
-
-  const comment = await cardRepository.getComment(id);
-
-  if(!comment){
-    res.status(404).json({ message: `comment not found: ${id}` });
-  } else if(username != comment.username) {
-    res.status(403).json({ message: `user is not autor`});
-  } else {
-    const update = await cardRepository.commentUpdate(id, text);
-    res.status(200).json(update);
-  }
-});
-
-router.delete('/:id', isAuth, async (req, res) => {
-  const id = req.params.id;
-  const username = req.user.username;
-
-  const comment = await cardRepository.getComment(req.params.id);
-
-  if(!comment){
-    res.status(404).json({ message: `comment not found: ${id}` });
-  } else if(username != comment.username) {
-    res.status(403).json({ message: `user is not autor`});
-  } else {
-    const remove = await cardRepository.commentRemove(id);
-    res.status(200).json(remove);
-  }
-});
+router.put('/:id', isAuth, commentController.updateComment);
+router.delete('/:id',isAuth, commentController.deleteComment);
 
 export default router;
